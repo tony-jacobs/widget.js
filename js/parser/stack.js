013c36a11132ceac953297861ab1e8cb4ac670e9
Tony Jacobs <tony@leverege.com>
2015-05-06 14:23:33 -0400

    If a ${} leads to a function instead of a traditional value, then try calling the function

--- js/parser/stack.js
+++ js/parser/stack.js
@@ -11,20 +11,23 @@
     
   function decode( token, context )
   {
+    var result;
     var sep = token.indexOf( ',' );
     var path;
     if( sep > -1 )
     {
       var dbName = token.substring( 0, sep );
       path = token.substring( sep+1 ).split( '|', 2 );
-      return widget.util.get( dbName, path[0], path[1] );
+      result = widget.util.get( dbName, path[0], path[1] );
     }
     else
     {
       context = context || (widget.util.getStack() || [])[0];
       path = token.split( '|', 2 );
-      return widget.get( context, path[0], path[1] ) || "${" + token + "}";
+      result = widget.get( context, path[0], path[1] ) || "${" + token + "}";
     }
+    
+    return $.isFunction(result) ? result.apply( context ) : result;
   }
 
 })();
