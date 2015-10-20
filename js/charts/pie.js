9ece803bb4b461ec9cc6fe37034d11f9cc0edf3e
Tony Jacobs <tony@leverege.com>
2015-10-20 01:16:58 -0400

    Add label height in pie chart to be configured.

--- js/charts/pie.js
+++ js/charts/pie.js
@@ -48,16 +48,34 @@
       pieChart.showLegend( options.showLegend===undefined ? true : options.showLegend );  
       pieChart.legendPosition( options.legendPosition===undefined ? 'top' : options.legendPosition );  
       pieChart.showLabels( options.showLabels===undefined ? true : options.showLabels );
+      
+      if( options.colors )
+        pieChart.color( options.colors );
 
       if( options.title )
         $( '<div/>', {html: options.title} ).addClass( 'chartTitle' ).prependTo( options.parent );
+
+      if( options.legendLineHeight )
+      {
+        pieChart.dispatch.on( 'renderEnd', function( model ){
+          var pie = d3.select( domSelector + ' .nv-pieChart' );
+          var labelHeight = pie.node().getBBox().height / 4;
+          var labels = d3.select( domSelector ).selectAll( domSelector + ' g.nv-series' );
+          
+          labels.each( function (d, i) {
+            d3.select(this).attr('transform', function () {
+              return 'translate( 0,' + (options.legendLineHeight/2 + (i * options.legendLineHeight)) + ')';
+            });
+          });
+        });
+      }
 
       d3.select( domSelector )
         .style( {width:width+'px', height:height+'px'} )
         .datum( dataProjection )
         .transition().duration( options.transtitionDuration||600 )
         .call( pieChart );
-  
+
       factory.charts[ chartId ] = pieChart;
       pieChart.domSelector = domSelector;
       chartPromise.resolve( pieChart );
