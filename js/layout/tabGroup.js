1bfee1d4bacd967a09918421ed78ef374b31f05d
Tony Jacobs <tjacobs@broadsoft.com>
2016-02-23 21:26:32 -0500

    whitespace

--- js/layout/tabGroup.js
+++ js/layout/tabGroup.js
@@ -1,5 +1,5 @@
 (function registerTabGroupLayout(){
-  
+
   widget.layout.register( 'tabGroup', createTabGroup, {
     description: "TODO!"
   } );
@@ -10,7 +10,7 @@
     var view = def.parent;
     var data = def.layout;
     var options = def.options;
-    
+
     var labelHolderClass = options.labelHolder || 'labels';
     var tabOptions = $.extend( {}, options, {
       tabData: data.content,
@@ -20,6 +20,7 @@
     var tabGroup = $( tabOptions.labelSelector, view );
     if( !tabGroup || !tabGroup.length )
       tabGroup = $('<div/>').addClass( labelHolderClass ).appendTo( view );
+
     var tabNav = widget.ui.generateTabs( view, tabOptions );
     var tabManager = tabNav.data('tabManager');
 
@@ -27,22 +28,22 @@
     var dataStack = def.stack || [data];
     if( def.data )
       dataStack.unshift( def.data );
-    
+
     $.each( data.content, function( i, item ) {
-      
+
       var itemData;
       if( item.data )
         itemData = item.data;
       else if( item.dataSource || def.layout.dataSource )
         itemData = item;
-      
+
       if( itemData )
         dataStack.unshift( itemData );
       widget.layout( item.view, item, {}, dataStack );
       if( itemData )
         dataStack.shift( itemData );
     } );
-    
+
     return tabGroup;
   }
 
